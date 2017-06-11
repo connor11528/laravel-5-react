f95f753a20230006d117473b28440a61eafd1283
Connor Leech <connorleech@gmail.com>
2017-06-10 21:25:17 -0700

    init app

---
+++ app/Http/Controllers/BusinessController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Business;
+use Illuminate\Http\Request;
+
+class BusinessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd('hello');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Business  $business
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Business $business)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Business  $business
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Business $business)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Business  $business
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Business $business)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Business  $business
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Business $business)
+    {
+        //
+    }
+}
